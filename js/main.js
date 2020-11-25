f158662f5200805c50ac71349027e26ffd01c21b
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-11-25 17:27:49 +0100

    entrar

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const capa = document.createElement('div');
+    document.body.appendChild(capa);
+
+    const boton = document.createElement('button');
+    document.body.appendChild(boton);
+
+
+
+
+
+});
